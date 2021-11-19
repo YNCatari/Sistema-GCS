e262921a4381f31d1387a548b6e32fa8d5290004
posi vargas <50784472+snpss10@users.noreply.github.com>
2021-11-18 23:29:12 -0500

    controladores para rodrigo

---
+++ SistemaGCS/SistemaGCS/Controllers/InformeEstadoController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SistemaGCS.Controllers
+{
+    public class InformeEstadoController : Controller
+    {
+        // GET: InformeEstado
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ SistemaGCS/SistemaGCS/Controllers/OrdenCambioDetalleController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SistemaGCS.Controllers
+{
+    public class OrdenCambioDetalleController : Controller
+    {
+        // GET: OrdenCambioDetalle
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
